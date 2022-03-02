331da520869e8fd83030463baffdfd2bd38c8d9b
Zach Keller <77516491+ZachKeller47@users.noreply.github.com>
2022-03-01 21:07:26 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+const navLogo = document.querySelector('.navbar__logo')
+
+// display mobile menu
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+// close mobile menu when clicking on a menu item
+
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if(window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
